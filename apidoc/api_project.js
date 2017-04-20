cde3b7970829ecabd275c9ef245241084ed23ee2
Alex Levshin <aleksander.levshin@gmail.com>
2017-04-20 18:34:07 +0300

    Upload apidoc data

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Workty",
+  "version": "1.0.0",
+  "description": "Workty Rest API",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-04-19T17:38:39.369Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
